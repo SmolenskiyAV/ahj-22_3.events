4297ac153033e80cde91793800230c07300c3ee8
SmolenskiyAV <JoJo_666@list.ru>
2022-04-10 22:45:42 +0700

    ver.1.0

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,10 @@ import { toggleDone } from './actions';
 
 playField();
 
-const holeGame = document.querySelector('.hole-game'); // поиск дочерних элементов внутри родителя class = "hole-game"
-holeGame.addEventListener('click', toggleDone); // обработка события клика на один из элементов внутри родителя <div class="hole-game">
-
+document.addEventListener('DOMContentLoaded', () => { // помещаем addEventlistener внутрь обратной функции - для успешного прохождения тестов Jest
+  const holeGame = document.querySelector('.hole-game'); // поиск дочерних элементов внутри родителя class = "hole-game"
+  holeGame.addEventListener('click', toggleDone); // обработка события клика на один из элементов внутри родителя <div class="hole-game">
+});
 // for demonstration purpose only
 export default function demo(value) {
   return value;
